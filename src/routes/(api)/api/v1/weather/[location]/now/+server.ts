c6f2ac6384ea6ec53d84112fa9c99e8696f04963
max-got <max.gottschalk@mailbox.org>
2024-07-19 09:31:36 +0200

    feat: Add server route for fetching current weather data

---
+++ src/routes/(api)/api/v1/weather/[location]/now/+server.ts
@@ -0,0 +1,59 @@
+export const prerender = true;
+
+import { json } from '@sveltejs/kit';
+import type { EntryGenerator, RequestHandler } from './$types';
+import { Zod_WeatherNow } from '$lib/server/data/api/weather.schema';
+
+import { AVAILABLE_SLUGS, type WeatherLocation } from '$lib/server/data/api/weather.routes';
+
+const WEATHER_API_URL = 'https://api.brightsky.dev' as const;
+
+const COORDINATES_MAP = {
+	potsdam: { lat: 52.39886, lon: 13.06566 },
+	hiddensee: { lat: 54.38333, lon: 13.08333 }
+} as const satisfies Record<WeatherLocation, { lat: number; lon: number }>;
+
+export const entries: EntryGenerator = () => {
+	return AVAILABLE_SLUGS.map((location) => ({ location }));
+};
+
+export const GET: RequestHandler = async ({ params, isSubRequest }) => {
+	// Only for internal requests
+	if (!isSubRequest) {
+		return json(
+			{
+				error: 'Invalid request'
+			},
+
+			{ status: 400 }
+		);
+	}
+	const { location } = params;
+
+	if (!AVAILABLE_SLUGS.includes(location as WeatherLocation)) {
+		return json(
+			{
+				error: 'Invalid location',
+				available_locations: AVAILABLE_SLUGS
+			},
+
+			{ status: 400 }
+		);
+	}
+
+	const { lat, lon } = COORDINATES_MAP[location as WeatherLocation];
+
+	const response = await fetch(
+		`${WEATHER_API_URL}/current_weather?lat=${lat}&lon=${lon}&units=dwd`
+	);
+
+	if (!response.ok) {
+		throw new Error('Weather API request failed');
+	}
+
+	const data = await response.json();
+
+	const parsed_data = Zod_WeatherNow.parse(data);
+
+	return json(parsed_data);
+};
